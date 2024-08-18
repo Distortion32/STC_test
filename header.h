b444773fc0ffbb29c3a91d5c66e10895b8c9a03a
Distortion32 <alexan.valit@yandex.ru>
2024-08-18 21:07:27 +0300

    header and readme update.

--- header.h
+++ header.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <array>
 
 // Структура для хранения координат точки
 struct point{
@@ -19,5 +20,16 @@ struct delayStruct{
     double AFC; //AF-CF
     double BFC; //BF-CF
 };
+
+std::array<double, 6> jakobianFunct(const std::array<double, 6> &Coordpoint, const point &d, const point &e, const point &f, const delayStruct &delay);
+// функция подсчета вектора частных производных
+std::array<double, 6> const_array_multi(const double &LR, const std::array<double, 6> &Jk);
+// функция почленного умножения вектора на константу
+std::array<double, 6> arraySubtraction(const std::array<double, 6> &a, const std::array<double, 6> &b);
+// функция вычисления разности векторов
+double arrayMod(const std::array<double, 6> &a);
+// вычисление модуля вектора
+double functForCheck(const std::array<double, 6> &Coordpoint, const point &d, const point &e, const point &f, const delayStruct &delay);
+// метод для проверки малости функции, которую нужно минимизировать
 
 #endif // HEADER_H
